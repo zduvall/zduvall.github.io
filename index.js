10882c0f9b2899b9cd5a000e8e04e4c75ee067fb
Zachary <duvallzt@gmail.com>
2021-03-24 20:25:16 -0600

    Create js file for contact form fetch

---
+++ index.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const contactForm = document.getElementById('contact-form');
+
+  contactForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const contactData = new FormData(contactForm);
+
+    const name = contactData.get('name');
+    const email = contactData.get('email');
+    const message = contactData.get('message');
+    const date = new Date();
+    const readableDate = date.toLocaleString('en-us', {
+      timeZone: 'America/Denver',
+    });
+    const data = {
+      name: name,
+      email: email,
+      message: message,
+      sent: readableDate,
+    };
+
+    await fetch(
+      'https://sheet.best/api/sheets/7b93a3e8-49c3-4b58-a004-936482aafc82',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      }
+    );
+
+    window.location = 'https://zduvall.github.io/';
+  });
+});
